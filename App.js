46c39c31a1728540f24cef6ea61c3044d7cf79db
vishalpwr <vishalvir8p@gmail.com>
2022-08-11 19:29:27 +0530

    google-signin

---
+++ App.js
@@ -0,0 +1,31 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import React from 'react';
+import { Text, StatusBar, StyleSheet, ScrollView } from 'react-native';
+import GoogleSignIn from './src/components/GoogleSignIn';
+
+GoogleSignin.configure({
+  webClientId: 'XXXX-XXXXXX.apps.googleusercontent.com',
+});
+
+const App = () => {
+
+  return (
+    <ScrollView contentContainerStyle={{ paddingBottom: 40 }} style={styles.container}>
+      <StatusBar backgroundColor={'white'} barStyle="dark-content" />
+      <Text style={styles.text}>Firebase Authentications</Text>
+      <GoogleSignIn />
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  text: {
+    fontSize: 16,
+  },
+})
+
+export default App;
